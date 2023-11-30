2f3df9d2d72ae32ea3aec70fa9ef422f7616eae7
CalShucha-ScooterSoftware <callen@scootersoftware.com>
2023-11-30 13:59:23 -0600

    Test commit

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ import * as os from 'node:os';
 let temporaryFiles: string[] = [];
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Your extension is activated the very first time the command is executed 
 export function activate(context: vscode.ExtensionContext) {
 
 	let leftPath = "";
